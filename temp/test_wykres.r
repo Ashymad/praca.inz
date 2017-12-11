6a6eddb460b4cbec1ee361e43fefc19d37f4d522
Ashymad <czilukim@o2.pl>
2017-12-11 11:32:56 +0100

    modified gitignore; testname is now a variable

--- temp/test_wykres.r
+++ temp/test_wykres.r
@@ -2,14 +2,16 @@ library(h5)
 library(ggplot2)
 library(reshape2)
 
+test_name <- "sin"
+
 f <- h5file("../tests/results.h5")
 
 results <- list(
-    julia=f["sin/julia"][],
-    octave=f["sin/octave"][],
-    matlab=f["sin/matlab"][],
-    scilab=f["sin/scilab"][][,],
-    python=f["sin/python"][]
+    julia=f[paste(test_name, "/julia", sep="")][],
+    octave=f[paste(test_name, "/octave", sep="")][],
+    matlab=f[paste(test_name, "/matlab", sep="")][],
+    scilab=f[paste(test_name, "/scilab", sep="")][][,],
+    python=f[paste(test_name, "/python", sep="")][]
 )
 stddev <- as.data.frame(unlist(lapply(results, function(vec) {
     sqrt(var(vec))
@@ -18,9 +20,9 @@ means <- as.data.frame(unlist(lapply(results, function(vec) {
     mean(vec)
 })))
 
-means["Software"] = rownames(means)
-means.molten = melt(means, id.vars="Software", value.name="ExecutionTime")
-means["Software"] = c()
+means["Software"] <- rownames(means)
+means.molten <- melt(means, id.vars="Software", value.name="ExecutionTime")
+means["Software"] <- c()
 
 ggplot(means.molten, aes(x=Software, y=ExecutionTime)) + geom_errorbar(aes(ymin=means-stddev, ymax=means+stddev), colour="red", width=.1) + geom_point()
 
