9a3d219e18cf006c1a1f72b82af55d84d0fff976
Ashymad <czilukim@o2.pl>
2017-12-11 01:04:15 +0100

    100 tests

--- tests/python/tests/sin/test.py
+++ tests/python/tests/sin/test.py
@@ -4,7 +4,7 @@ import math as m
 import numpy as np
 
 test_name = "sin"
-number_of_tests = 10
+number_of_tests = 100
 
 x = np.arange(0,2*m.pi,0.0000001)
 
--- tests/scilab/tests/sin/test.sci
+++ tests/scilab/tests/sin/test.sci
@@ -1,7 +1,7 @@
 // Sinus test
 
 test_name = "sin";
-number_of_tests = 10;
+number_of_tests = 100;
 
 x = 0:0.0000001:2*%pi;
 
