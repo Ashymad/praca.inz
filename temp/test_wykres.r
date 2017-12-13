40c0e95e1f3dc229c1e0e388d89cf8f9024c7ac6
Ashymad <czilukim@o2.pl>
2017-12-14 00:47:08 +0100

    Nowy wykres

--- temp/test_wykres.r
+++ temp/test_wykres.r
@@ -1,28 +1,37 @@
 library(h5)
 library(ggplot2)
 library(reshape2)
+library(dplyr)
 
-test_name <- "fft"
+test_name <- "sin"
 
 f <- h5file("../tests/results.h5")
 
 results <- list(
     julia=f[paste(test_name, "/julia", sep="")][],
-    octave=f[paste(test_name, "/octave", sep="")][][,],
+    octave=f[paste(test_name, "/octave", sep="")][],
     matlab=f[paste(test_name, "/matlab", sep="")][],
-    scilab=f[paste(test_name, "/scilab", sep="")][][,],
+    scilab=f[paste(test_name, "/scilab", sep="")][],
     python=f[paste(test_name, "/python", sep="")][]
 )
-stddev <- as.data.frame(unlist(lapply(results, function(vec) {
-    sqrt(var(vec))
-})))
-means <- as.data.frame(unlist(lapply(results, function(vec) {
-    mean(vec)
-})))
 
-means["Software"] <- rownames(means)
-means.molten <- melt(means, id.vars="Software", value.name="ExecutionTime")
-means["Software"] <- c()
+rowVars <- function(x) {
+  rowSums((x - rowMeans(x))^2)/(dim(x)[2] - 1)
+}
 
-ggplot(means.molten, aes(x=Software, y=ExecutionTime)) + geom_errorbar(aes(ymin=means-stddev, ymax=means+stddev), colour="red", width=.1) + geom_point()
+stddev <- lapply(results, function(vec) {
+    sqrt(rowVars(vec))
+})
+means <- lapply(results, function(vec) {
+    rowMeans(vec)
+})
+
+means.molten <- as.data.frame(melt(means) %>% group_by(L1) %>% mutate(id = row_number()))
+stddev.molten <- as.data.frame(melt(stddev) %>% group_by(L1) %>% mutate(id = row_number()))
+
+yMin <- means.molten["value"] - stddev.molten["value"]
+yMin[yMin < 1e-10] <- 1e-10
+yMax <- means.molten["value"] + stddev.molten["value"]
+
+ggplot(means.molten, aes(x=id, y=value, colour=L1)) + geom_errorbar(aes(ymin=yMin, ymax=yMax), width=.1) + scale_y_log10(limits=c(1e-10,NA)) + geom_point() + geom_line()
 
