6b6a5249717d5dbb6292f51d864aac991a5302ab
Ashymad <czilukim@o2.pl>
2017-12-17 16:39:36 +0100

    separate size tests for Julia

--- tests/julia/tester.jl
+++ tests/julia/tester.jl
@@ -5,24 +5,22 @@ include("test.jl");
 
 ## Testing
 
-results = zeros(number_of_tests, max_input_size);
+results = zeros(number_of_tests);
 
-for input_size = 1:max_input_size
-	input_data = prepare_input(10^input_size)
-	output_data = test_function(input_data)
+input_data = prepare_input(10^input_size)
+output_data = test_function(input_data)
 
-	for i = 1:number_of_tests
-		tic();
-		output_data = test_function(input_data);
-		results[i, input_size] = toq();
-	end
+for i = 1:number_of_tests
+	tic();
+	output_data = test_function(input_data);
+	results[i] = toq();
 end
 
 # Saving to file
 using HDF5
 
 file = "/tmp/results.h5";
-dataset = "$test_name/julia";
+dataset = "$test_name/julia/$input_size";
 
 h5open(file, "r+") do fid
 	if exists(fid, dataset);
