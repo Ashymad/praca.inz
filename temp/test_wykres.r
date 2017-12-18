fa197ebb319e68710751a220489450f6fd680624
Ashymad <czilukim@o2.pl>
2017-12-18 11:57:36 +0100

    Fix accidental results sorting.

--- temp/test_wykres.r
+++ temp/test_wykres.r
@@ -16,7 +16,7 @@ for (package in packages) {
     results[[package]] <- matrix(0,max_input_size,length(f[test_name][package]["1"][])-removetop)
     for (input_size in 1:max_input_size) {
         res <- as.vector(f[test_name][package][toString(input_size)][])
-        results[[package]][input_size,] <- res[head(order(res),-removetop)]
+        results[[package]][input_size,] <- res[-tail(order(res),removetop)]
     }
 }
 
