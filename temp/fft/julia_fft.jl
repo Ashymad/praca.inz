2fdfe24289cd2964783bb8356a45301eaac1e639
Ashymad <czilukim@o2.pl>
2017-12-22 20:41:05 +0100

    Correct indent

--- temp/fft/julia_fft.jl
+++ temp/fft/julia_fft.jl
@@ -1,44 +1,44 @@
 function four1(data::Array{T,1}, isign::Int64 = 1) where {T<:Number}
-    nn = length(data)
-    nnlog = log2(nn)
-    if ceil(nnlog) != nnlog
-	    error("Length of data vector must be a power of 2.")
-    end
-    j::Int64 = 1
-    res::Array{Complex{Float64},1} = copy(data)
+	nn = length(data)
+	nnlog = log2(nn)
+	if ceil(nnlog) != nnlog
+		error("Length of data vector must be a power of 2.")
+	end
+	j::Int64 = 1
+	res::Array{Complex{Float64},1} = copy(data)
 
-    for i = 1:nn
-        if j > i
-            res[j], res[i] = data[i], data[j]
-        end
-        m = nn/2
-        while m >= 2 && j > m
-            j -= m
-            m = m/2
-        end
-        j += m
-    end
+	for i = 1:nn
+		if j > i
+			res[j], res[i] = data[i], data[j]
+		end
+		m = nn/2
+		while m >= 2 && j > m
+			j -= m
+			m = m/2
+		end
+		j += m
+	end
 
-    mmax = 1;
-    while nn > mmax
-        istep = 2*mmax
-        theta = isign*(pi/mmax)
-        wp = -2*sin(theta/2)^2 + sin(theta)*im
-        w = 1 + 0im
+	mmax = 1;
+	while nn > mmax
+		istep = 2*mmax
+		theta = isign*(pi/mmax)
+		wp = -2*sin(theta/2)^2 + sin(theta)*im
+		w = 1 + 0im
 
-        for m = 1:mmax
-            for i = m:istep:nn
-                j = i + mmax
-                temp = w*res[j]
-                res[j] = res[i] - temp
-                res[i] += temp
-            end
-            w += w*wp
-        end
-        mmax = istep
-    end
+		for m = 1:mmax
+			for i = m:istep:nn
+				j = i + mmax
+				temp = w*res[j]
+				res[j] = res[i] - temp
+				res[i] += temp
+			end
+			w += w*wp
+		end
+		mmax = istep
+	end
 
-    res
+	res
 end
 
 input_size = 2048
--- temp/fft/matlab_fft.m
+++ temp/fft/matlab_fft.m
@@ -13,43 +13,43 @@ four1 = @(data, isign) four1_(data, isign); % This allows calling the function i
 % Y = four1(X, 1); % Only possible in MATLAB
 
 function res = four1_(data, isign)
-    nn = length(data);
-    nnlog = log2(nn);
-    if ceil(nnlog) ~= nnlog
+nn = length(data);
+nnlog = log2(nn);
+if ceil(nnlog) ~= nnlog
 	    error("Length of data vector must be a power of 2.");
     end
     j = 1;
     res = data + 0i;
 
     for i = 1:nn
-        if j > i
-            [res(j), res(i)] = deal(data(i), data(j));
-        end
-        m = nn/2;
-        while m >= 2 && j > m
-            j = j - m;
-            m = m/2;
-        end
-        j = j + m;
+	    if j > i
+		    [res(j), res(i)] = deal(data(i), data(j));
+	end
+	m = nn/2;
+	while m >= 2 && j > m
+		j = j - m;
+		m = m/2;
+	end
+	j = j + m;
+end
+
+mmax = 1;
+while nn > mmax
+	istep = 2*mmax;
+	theta = isign*(pi/mmax);
+	wp = -2*sin(theta/2)^2 + sin(theta)*1i;
+	w = 1 + 0i;
+
+	for m = 1:mmax
+		for i = m:istep:nn
+			j = i + mmax;
+			temp = w*res(j);
+			res(j) = res(i) - temp;
+			res(i) = res(i) + temp;
+	    end
+	    w = w + w*wp;
     end
-
-    mmax = 1;
-    while nn > mmax
-        istep = 2*mmax;
-        theta = isign*(pi/mmax);
-        wp = -2*sin(theta/2)^2 + sin(theta)*1i;
-        w = 1 + 0i;
-
-        for m = 1:mmax
-            for i = m:istep:nn
-                j = i + mmax;
-                temp = w*res(j);
-                res(j) = res(i) - temp;
-                res(i) = res(i) + temp;
-            end
-            w = w + w*wp;
-        end
-        mmax = istep;
+    mmax = istep;
     end
 end
 
