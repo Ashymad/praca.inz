b5c4fa8b40d54a6b58520cc7ab3be08a5d7ca2a3
Ashymad <czilukim@o2.pl>
2017-12-28 19:30:15 +0100

    Remove comments.

--- tests/test.sh
+++ tests/test.sh
@@ -23,12 +23,12 @@ GOPTIONS=("number_of_tests=$NUMBER_OF_TESTS")
 cp results.h5 /tmp
 
 testdir () {
-	cd $STARTDIR/$3 #Przejdź do folderu zawierającego testy
-	#GOPTIONS - Tablica zawierająca opcje globalne
-	for option in ${GOPTIONS[*]}; do  #np. ilość testów
+	cd $STARTDIR/$3
+
+	for option in ${GOPTIONS[*]}; do
 		echo "$option;" >> options.$2
-	done #Te opcje zapisywane są w pliku
-	
+	done
+
 	for dir in tests/*; do
 		cd $STARTDIR/$3
 		TESTNAME=$(echo $dir | cut -c 7-)
