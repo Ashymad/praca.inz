d58e1e2e0b3353baff612e3c21aeef6f5cadc4e4
Ashymad <czilukim@o2.pl>
2017-12-30 00:04:21 +0100

    Remove comments.

--- tests/python/tests/fft/test.py
+++ tests/python/tests/fft/test.py
@@ -3,11 +3,11 @@ import numpy as np
 from scipy import fftpack
 import math as m
 
-Fs = 1000            # Sampling frequency                    
-T = 1/Fs             # Sampling period       
+Fs = 1000
+T = 1/Fs
 
 def prepare_input(input_size):
-    t = np.arange(0,input_size)*T        # Time vector
+    t = np.arange(0,input_size)*T
 
     S = 0.7*np.sin(2*m.pi*50*t) + np.sin(2*m.pi*120*t)
     return S + 2*np.random.randn(np.size(t))
