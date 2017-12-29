ccd1b54d64612727c8fd334a6cec3688c7cba041
Ashymad <czilukim@o2.pl>
2017-12-29 01:04:38 +0100

    Remove comments.

--- tests/matlab/tests/fft/test.m
+++ tests/matlab/tests/fft/test.m
@@ -1,13 +1,13 @@
 % FFT test
 
-Fs = 1000;            % Sampling frequency                    
-T = 1/Fs;             % Sampling period       
-
 prepare_input = @(input_size) prepare_input_(input_size, T);
 test_function = @(input_data) test_function_(input_data);
 
+Fs = 1000;
+T = 1/Fs;
+
 function input_data = prepare_input_(input_size, T)
-	t = (0:input_size-1)*T;        % Time vector
+	t = (0:input_size-1)*T;
 	S = 0.7*sin(2*pi*50*t) + sin(2*pi*120*t);
 	input_data = S + 2*randn(size(t));
 end
